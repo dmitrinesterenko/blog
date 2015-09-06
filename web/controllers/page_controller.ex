fc10104a03787d1dc02a71569ad6bd37d9df253e
Dmitri Nesterenko <dneseterenko@xogrp.com>
2015-09-06 18:07:31 -0500

    Complete the about page.
    Signed-off-by: Dmitri Nesterenko <dneseterenko@xogrp.com>

--- web/controllers/page_controller.ex
+++ web/controllers/page_controller.ex
@@ -6,4 +6,8 @@ defmodule BlogPhoenix.PageController do
   def index(conn, _params) do
     render conn, "index.html"
   end
+  
+  def about(conn, _params) do
+    render conn, "about.html"
+  end
 end
