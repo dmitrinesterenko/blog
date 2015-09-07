80a1715f1475be1210a77992c3366a50e2383633
Dmitri Nesterenko <dneseterenko@xogrp.com>
2015-09-06 22:28:17 -0500

    WIP. Add routes, controller and views for writing a new text.
    Signed-off-by: Dmitri Nesterenko <dneseterenko@xogrp.com>

---
+++ web/controllers/writer_controller.ex
@@ -0,0 +1,33 @@
+defmodule BlogPhoenix.WriterController do
+  use BlogPhoenix.Web, :controller
+
+  plug :action
+
+  def index(conn, _params) do
+    render conn,"index.html"
+  end
+
+  def new(conn, _params) do
+    render conn, "new.html"
+  end
+  
+  def create(conn, params) do
+   # writer = Writer.new(params[:title], params[:slug], params[:body])
+    #message = writer.save
+   message = "" 
+   if message == "" do
+      message = "Saved"
+   end
+
+   # This does not print out an :info message
+   # TODO investigate why it does not 
+   # Time to debug and use get_flash :info
+   #put_flash conn, :info,  "Saved"
+   # This is one way of rendering
+   #render conn, "new.html"
+   # This is another way of doing multiple actions with a connection
+   conn
+    |> put_flash(:info, message)
+    |> render "new.html" #, info: message
+  end
+end
---
+++ web/views/writer_view.ex
@@ -0,0 +1,3 @@
+defmodule BlogPhoenix.WriterView do
+  use BlogPhoenix.Web, :view
+end
