9378201a5d4ffdb507fdd6cd0a2673c7e38ebdf0
dmitrinesterenko <dneseterenko@xogrp.com>
2016-08-06 19:58:24 -0400

    Fixes the reader view to pass in the full post
    ReaderView should work on the whole Post model
    Adds tests for the indexing view and preview field
    Remove junk copy from the reader view

---
+++ test/views/reader_view_test.exs
@@ -0,0 +1,46 @@
+defmodule BlogPhoenix.ReaderViewTest do
+  alias BlogPhoenix.Post
+  alias BlogPhoenix.ReaderView
+  use BlogPhoenix.ConnCase, async: false
+
+  # Bring render/3 and render_to_string/3 for testing custom views
+  import Phoenix.View
+
+  @posts [%Post{title: "the Narrative of The Man From Narangasett",
+        body: "The winter falls were almost frozen. Moving slowly like the line
+at the bakery next to the even more life retardant post office. It was a Tuesday
+in February"},
+        %Post{title: "life and Times of Weiner Stefanner",
+body: "A curious little boy with a lop sided ear Weiner was always gregariously
+in the way of his father who would have rather had another table that clealy
+belonged to his pipe tobacco and accoutrements rather than this chubby little
+grub whose ears made him seem as if he is hearing something that no one else
+can. Matilde was impressed of course that she could pull off such an act of
+angelic mischief and would make up stories about each of their neighbors when
+Weiner's ear would point toward their town house in their tight little grouping
+that surrounded if not the main but definitely the most picturesque square in
+town."}
+]
+
+  test "index view" do
+    assert render_to_string(ReaderView, "index.html", posts: @posts) =~
+    "winter falls"
+  end
+
+ # test "read view" do
+ #   {status, post} = Enum.fetch(@posts, 1)
+ #   assert render_to_string(ReaderView, "reader.html", post: post) =~
+ #   "A curious little boy with a lop sided ear"
+ # end
+
+  test "title" do
+    {_status, post} = Enum.fetch(@posts, 0)
+    assert ReaderView.title(post) == "THE NARRATIVE OF THE MAN FROM NARANGASETT"
+  end
+
+  test "preview" do
+    {_status, post} = Enum.fetch(@posts, 1)
+    assert ReaderView.preview(post) =~ "A curious little boy"
+  end
+
+end
