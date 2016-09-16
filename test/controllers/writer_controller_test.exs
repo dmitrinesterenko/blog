a23a3d2e3db26d19ba4fc40acdde394e66524208
dmitrinesterenko <dneseterenko@xogrp.com>
2016-09-15 22:50:49 -0400

    WIP. Adds a test for the update functionality

--- test/controllers/writer_controller_test.exs
+++ test/controllers/writer_controller_test.exs
@@ -2,6 +2,10 @@ require IEx
 defmodule BlogPhoenix.WriterControllerTest do
   use BlogPhoenix.ConnCase
 
+  setup do
+    {:ok, [title: "title", body: "body", id: "1", "new_body": "body 2"]}
+  end
+
   test "GET /write" do
     conn = get build_conn(), "/write"
     assert html_response(conn, 200) =~ "TITLE"
@@ -16,4 +20,10 @@ defmodule BlogPhoenix.WriterControllerTest do
     conn = post build_conn(), "/write", [body: "test body"]
     assert html_response(conn, 422) =~ Phoenix.HTML.Safe.to_iodata("title can't be blank")
   end
+
+  test "PUT /write with correct parameters", context do
+    conn = put build_conn(), "/write", [id: context[:id], title: context[:title], body: context[:new_body]]
+    assert html_response(conn, 302) =~ "redirected"
+  end
+
 end
